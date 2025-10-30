d99e8b410fea4ba806b9de58ef618a2a7f9dfabe
Anthony Schurle <legoanthonyjr@gmail.com>
2025-10-29 20:38:40 -0500

    FID (Messy, no commands, 1 trial)

--- backend/GAN/run.py
+++ backend/GAN/run.py
@@ -3,50 +3,110 @@ from . import generator as gen, discriminator as dis, device as dev, train
 from PIL import Image
 import torch
 from torch import nn
-#from pytorch_fid.fid_score import calculate_fid_given_paths
-import matplotlib.pyplot as plt
+import torchvision
+from pytorch_fid import fid_score
 import os
 
-def run():
+def run(batch_size: int = 32):
+    transform = torchvision.transforms.Compose(
+        [torchvision.transforms.ToTensor(), torchvision.transforms.Normalize((0.5,), (0.5,))]
+    )
+    train_set = torchvision.datasets.MNIST( root=".", train=True, download=True, transform=transform)
+
+    label_to_idx = dict()
+    for idx, (_, label) in enumerate(train_set):
+        label = label
+        if label not in label_to_idx.keys():
+            label_to_idx[label] = []
+        label_to_idx[label].append(idx)
+
     device = dev.Device().get_device()
-    generator = gen.Generator().to(device=device)
-    discriminator = dis.Discriminator().to(device=device)
-    batch_size = 32
-    train.Train.train(discriminator, generator, nn.BCELoss(), device, batch_size = batch_size)
 
-    generate_srng(generator, batch_size, device)
-    generate_srng(generator, batch_size, device)
+    os.makedirs("out/fid", exist_ok=True)
+    srng_fid_seq0 = []
+    srng_fid_seq1 = []
+    srng_fid_seq2 = []
+    trng_fid_seq = []
+    for label in range(10):
+        print(f"GAN Label - {label}:\n")
+        train_subset = torch.utils.data.Subset(train_set, label_to_idx[label])
+        generator = gen.Generator().to(device=device)
+        discriminator = dis.Discriminator().to(device=device)
 
-def generate_srng(generator, batch_size, device):
-    os.makedirs("out", exist_ok=True)
+        train.Train.train(discriminator, generator, nn.BCELoss(), device, 0.0001, 500, batch_size, train_subset)
 
-    latent_space_samples = torch.randn(batch_size, 100).to(device=device)
+        srng_path0 = f"out/srng0/label-{label}"
+        srng_path1 = f"out/srng1/label-{label}"
+        srng_path2 = f"out/srng2/label-{label}"
+        trng_path = f"out/trng/label-{label}"
+        train_path = f"out/training/label-{label}"
+        generate_srng(generator, 300, device, srng_path0, 67)
+        generate_srng(generator, 300, device, srng_path1, 69)
+        generate_srng(generator, 300, device, srng_path2, 222)
+        generate_trng(generator, 300, device, trng_path)
+        show_training_data(train_subset, f"out/training/label-{label}")
+
+        print("Calculating FID...")
+        srng_fid_seq0.insert(0, fid_score.calculate_fid_given_paths([train_path, srng_path0], batch_size, device=device.type, dims=2048))
+        srng_fid_seq1.insert(0, fid_score.calculate_fid_given_paths([train_path, srng_path1], batch_size, device=device.type, dims=2048))
+        srng_fid_seq2.insert(0, fid_score.calculate_fid_given_paths([train_path, srng_path2], batch_size, device=device.type, dims=2048))
+        trng_fid_seq.insert(0, fid_score.calculate_fid_given_paths([train_path, trng_path], batch_size, device=device.type, dims=2048))
+
+    with open("out/fid/fid.txt", "w") as file:
+        for label in range(len(srng_fid_seq0)):
+            file.write(f"Label - {label}:\n")
+            file.write(f"\tSRNG - {srng_fid_seq0[label]}\n")
+            file.write(f"\tSRNG - {srng_fid_seq1[label]}\n")
+            file.write(f"\tSRNG - {srng_fid_seq2[label]}\n")
+            file.write(f"\tTRNG - {trng_fid_seq[label]}\n")
+        file.write("Average:\n")
+        file.write(f"SRNG - {sum(srng_fid_seq0) / len(srng_fid_seq0)}\n")
+        file.write(f"SRNG - {sum(srng_fid_seq1) / len(srng_fid_seq1)}\n")
+        file.write(f"SRNG - {sum(srng_fid_seq2) / len(srng_fid_seq2)}\n")
+        file.write(f"TRNG - {sum(trng_fid_seq) / len(trng_fid_seq)}\n")
+
+def generate_srng(generator, gen_size, device, output_dir, seed):
+    os.makedirs(output_dir, exist_ok=True)
+
+    torch.manual_seed(seed)
+    latent_space_samples = torch.randn(gen_size, 100).to(device=device)
     generated_samples = generator(latent_space_samples)
     generated_samples = generated_samples.cpu().detach()
 
-    for i in range(batch_size):
+    for i in range(gen_size):
         img_tensor = generated_samples[i].reshape(28, 28)
         img_array = ((img_tensor - img_tensor.min()) / (img_tensor.max() - img_tensor.min()) * 255).numpy().astype('uint8')
-        img = Image.fromarray(img_array, mode='L')
-        img.save(os.path.join("out", f"srng_{i}.png"))
+        img = Image.fromarray(img_array, mode='L').convert("RGB")
+        img.save(os.path.join(output_dir, f"srng_{i}.png"))
+    
+    return generated_samples
 
-def generate_trng(generator, batch_size, device):
-    os.makedirs("out", exist_ok=True)
+def generate_trng(generator, gen_size, device, output_dir):
+    os.makedirs(output_dir, exist_ok=True)
 
-    """
-    #Replace with better random latent space
     with open("./gaussian.txt", "r") as f:
         numbers = [float(line.strip()) for line in f]
     numbers_tensor = torch.tensor(numbers, dtype=torch.float32, device=device)
-    numbers_tensor = numbers_tensor[:batch_size * 100]
-    latent_space_samples = numbers_tensor.view(batch_size, 100)
-    generated_samples = generator(latent_space_samples)
+    numbers_tensor = numbers_tensor[:gen_size * 100]
+    latent_space_samples = numbers_tensor.view(gen_size, 100)
 
+    generated_samples = generator(latent_space_samples)
     generated_samples = generated_samples.cpu().detach()
-    for i in range(16):
-        ax = plt.subplot(4, 4, i + 1)
-        plt.imshow(generated_samples[i].reshape(28, 28), cmap="gray_r")
-        plt.xticks([])
-        plt.yticks([])
-    plt.show()
-    """
+
+    for i in range(gen_size):
+        img_tensor = generated_samples[i].reshape(28, 28)
+        img_array = ((img_tensor - img_tensor.min()) / (img_tensor.max() - img_tensor.min()) * 255).numpy().astype('uint8')
+        img = Image.fromarray(img_array, mode='L').convert("RGB")
+        img.save(os.path.join(output_dir, f"trng_{i}.png"))
+    
+    return generated_samples
+
+def show_training_data(data, output_dir):
+    os.makedirs(output_dir, exist_ok=True)
+
+    for i in range(len(data)):
+        img_tensor, label = data[i]
+        img_tensor = img_tensor.squeeze(0)
+        img_array = ((img_tensor - img_tensor.min()) / (img_tensor.max() - img_tensor.min()) * 255).numpy().astype('uint8')
+        img = Image.fromarray(img_array, mode='L').convert("RGB")
+        img.save(os.path.join(output_dir, f"training_{i}.png"))
--- backend/GAN/train.py
+++ backend/GAN/train.py
@@ -1,28 +1,16 @@
 import torch
-import torchvision
 
 class Train:
-    def train(discriminator, generator, loss_function, device, learning_rate = 0.0001, num_epochs = 100, batch_size = 32):
+    def train(discriminator, generator, loss_function, device, learning_rate, num_epochs, batch_size, train_set):
         optimizer_discriminator = torch.optim.Adam(discriminator.parameters(), lr=learning_rate)
         optimizer_generator = torch.optim.Adam(generator.parameters(), lr=learning_rate)
-
-        #Transform that essentially subtracts all data by 0.5 and divides by 0.5 creating a range of [-1, 1]
-        transform = torchvision.transforms.Compose(
-            [torchvision.transforms.ToTensor(), torchvision.transforms.Normalize((0.5,), (0.5,))]
-        )
-
-        #Get the training set of numbers of 28x28 pixels
-        train_set = torchvision.datasets.MNIST(
-            root=".", train=True, download=True, transform=transform
-        )
-
         train_loader = torch.utils.data.DataLoader(
-            train_set, batch_size=batch_size, shuffle=True
+            train_set, batch_size=batch_size, shuffle=True, drop_last=True
         )
 
         print("Training...")
         for epoch in range(num_epochs):
-            for batch_num, (real_samples, mnist_labels) in enumerate(train_loader):
+            for batch_num, (real_samples, labels) in enumerate(train_loader):
                 # Data for training the discriminator
                 real_samples = real_samples.to(device=device)
                 real_samples_labels = torch.ones((batch_size, 1)).to(
@@ -64,7 +52,7 @@ class Train:
                 loss_generator.backward()
                 optimizer_generator.step()
 
-                # Show loss
-                if batch_num == batch_size - 1:
-                    print(f"Epoch: {epoch} Loss D.: {loss_discriminator}")
-                    print(f"Epoch: {epoch} Loss G.: {loss_generator}")
+            # Show loss
+            if epoch % 10 == 0:
+                print(f"Epoch: {epoch} Loss D.: {loss_discriminator}")
+                print(f"Epoch: {epoch} Loss G.: {loss_generator}")
